e852a591dbb948367ec36139aa61eb904e4cd382
abdelbasset4 <benatmani1999@gmail.com>
2023-03-11 21:15:32 +0100

    create teacher model and modify login service

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const schoolRoute = require('./dashboardRoute')
 const mountRoutes = (app)=>{
     app.use('/dashboard',schoolRoute)
     app.use('/auth',authRoute)
-    // app.use('/schools',schoolRoute)
 }
 
 module.exports = mountRoutes
