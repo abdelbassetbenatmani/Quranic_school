b5092e552616292dbeda05cff7e564c923351062
Sidben99 <sidben9991@gmail.com>
2023-03-17 23:19:39 +0100

    add getLoginPage

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const {login,signup,forgotPassword,verifyPassResetCode,resetPassword} = require('../services/authService')
-const {loginValidator} = require('../utils/validator/authValidator')
+const {
+  login,
+  signup,
+  forgotPassword,
+  verifyPassResetCode,
+  resetPassword,
+  getLoginPage,
+} = require('../services/authService');
+const { loginValidator } = require('../utils/validator/authValidator');
 
-
-router.post('/signup',signup)
-router.post('/login',loginValidator,login)
-router.post('/forgotpassword',forgotPassword)
-router.post('/verifyResetCode',verifyPassResetCode)
-router.post('/resetPassword',resetPassword)
+router.post('/signup', signup);
+router.route('/login').get(getLoginPage).post(loginValidator, login);
+router.post('/forgotpassword', forgotPassword);
+router.post('/verifyResetCode', verifyPassResetCode);
+router.post('/resetPassword', resetPassword);
 // router.post('/activateAccount',protect,allowedTo('admin'),activateAccount)
 
 module.exports = router;
