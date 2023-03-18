ddeb51b31a3c182ca240db09fe17dffb2ea80868
Sidben99 <sidben9991@gmail.com>
2023-03-18 14:54:54 +0100

    tweak password infos

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,41 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const teacherSchema = mongoose.Schema({
-    username:{
-        type:String,
-        require:[true,'username is required'],
-        trim:true,
-        unique:[true,"username is unique"]
+const teacherSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: [true, 'username is required'],
+      trim: true,
+      unique: [true, 'username is unique'],
     },
 
-    slug:{
-        type: String,
-        lowercase:true
+    slug: {
+      type: String,
+      lowercase: true,
     },
 
-    phone:String,
-    password:{
-        type: String,
-        required:[true,'password is required'],
-        minlength:6,
+    phone: String,
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      minlength: 6,
     },
-    passwordChangedAt:Date,
-    passwordResetCode:String,
-    passwordResetCodeExpired:Date,
-    passwordResetCodeVerify:Boolean,
-    role:{
-        type: String,
-        enum:['teacher','admin'],
-        default: 'teacher',
+    passwordInfos: {
+      passwordChangedAt: Date,
+      passwordResetCode: String,
+      passwordResetCodeExpired: Date,
+      passwordResetCodeVerify: Boolean,
     },
-  
 
-},{timestamps: true});
+    role: {
+      type: String,
+      enum: ['teacher', 'admin'],
+      default: 'teacher',
+    },
+  },
+  { timestamps: true }
+);
 
-
-const User = mongoose.model('User',teacherSchema);
-module.exports = User
+const User = mongoose.model('User', teacherSchema);
+module.exports = User;
