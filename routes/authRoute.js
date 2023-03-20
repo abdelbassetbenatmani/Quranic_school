c18ef42a964add64c8c434dc836649fc2ad3304e
Sidben99 <sidben9991@gmail.com>
2023-03-20 22:08:03 +0100

    add logout functionality

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,14 +8,18 @@ const {
   verifyPassResetCode,
   resetPassword,
   getLoginPage,
+  logout,
+  protect,
 } = require('../services/authService');
 const { loginValidator } = require('../utils/validator/authValidator');
 
-router.post('/signup', signup);
 router.route('/login').get(getLoginPage).post(loginValidator, login);
+router.use(protect);
+router.post('/logout', logout);
 router.post('/forgotpassword', forgotPassword);
 router.post('/verifyResetCode', verifyPassResetCode);
 router.post('/resetPassword', resetPassword);
+
 // router.post('/activateAccount',protect,allowedTo('admin'),activateAccount)
 
 module.exports = router;
