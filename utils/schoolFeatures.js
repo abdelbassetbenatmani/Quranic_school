942ca22bfe52f79066293d5c60e8d6c399e1e545
Sidben99 <sidben9991@gmail.com>
2023-03-20 22:08:43 +0100

    create schoolFeatures

---
+++ utils/schoolFeatures.js
@@ -0,0 +1,33 @@
+const School = require('../models/schoolModel');
+
+class SchoolFeatures {
+  constructor(userId) {
+    this.userId = userId;
+  }
+  getSchool = async function () {
+    const school = await School.findOne({ teacher: this.userId });
+    return school;
+  };
+  getTeachers = async function (filterCallback = (e) => e) {
+    const school = await this.getSchool();
+    const { teachers } = school;
+    return teachers.filter(filterCallback);
+  };
+  getStudents = async function (filterCallback = (e) => e) {
+    const school = await this.getSchool();
+    const { students } = school;
+    return students.filter(filterCallback);
+  };
+
+  getNumberOfStudents = async function (filterCallback = (e) => e) {
+    const students = await this.getStudents(filterCallback);
+    return students.length;
+  };
+
+  getNumberOfTeachers = async function (filterCallback = (e) => e) {
+    const teachers = await this.getTeachers(filterCallback);
+    return teachers.length;
+  };
+}
+
+module.exports = SchoolFeatures;
