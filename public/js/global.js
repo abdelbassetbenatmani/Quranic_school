c636ae704864bb7fe528a4fb729e3f2eb35c0533
abdelbasset4 <benatmani1999@gmail.com>
2023-04-01 01:25:46 +0200

    show teacher list in own school

--- public/js/global.js
+++ public/js/global.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('.menu')
 const sideBar = document.querySelector('.side-bar')
 menu.addEventListener('click',()=>{
-    console.log("he");
     menu.classList.toggle('white-menu')
     sideBar.classList.toggle('show-bar')
 })
