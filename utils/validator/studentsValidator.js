e3722bfb5d61c434a422b40c44d1f368f3d4f8ab
abdelbasset4 <benatmani1999@gmail.com>
2023-05-09 14:47:54 +0200

    add validation layer for create student

--- utils/validator/studentsValidator.js
+++ utils/validator/studentsValidator.js
@@ -2,11 +2,12 @@ const { check, body } = require('express-validator');
 const validatorMiddleware = require('../../middleware/validatorMiddleware');
 exports.addStudentValidator = [
   check('fullName').notEmpty().withMessage('الاسم الكامل اجباري'),
-  check('type').notEmpty().withMessage('الحالة اجبارية'),
-  //check('grade').notEmpty().withMessage('يرجى تحديد الرتبة'),
-  check('registrationDate')
-    .notEmpty()
-    .withMessage('يرجى تحديد تاريخ التعيين'),
+  check('BirthDate').notEmpty().withMessage('تاريخ الميلاد إجباري'),
+  check('sex').notEmpty().withMessage('الجنس إجباري'),
+  check('fatherName').notEmpty().withMessage('اسم الأب إجباري'),
+  check('schoolStatus').notEmpty().withMessage('الوضعية مع المدرسة التربوية إجباري'),
+  check('level').notEmpty().withMessage('المستوى التعليمي  إجباري'),
+  check('quranSave').notEmpty().withMessage('مستوى حفظ القرآن إجباري'),
   validatorMiddleware(),
 ];
 
